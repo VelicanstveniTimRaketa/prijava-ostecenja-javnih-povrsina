0349a0b1d7560a8456dfa8343f97eba53906b044
spinzed <davor.najev@gmail.com>
2023-10-29 17:49:35 +0100

    front: feat: Add login page mockup

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,17 +1,48 @@
-import { Layout, Button } from "antd";
-import { Header } from "antd/es/layout/layout";
-import { Link } from "react-router-dom";
+import { Layout, Button, Typography, Form, Input } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+import { useNavigate } from "react-router-dom";
+import Logo from "../components/Logo";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <Layout>
-      <Header style={{ borderBottom: 0, borderTop: "2px", backgroundColor: "white", display: "flex", alignItems: "center", justifyContent: "end" }}>
-        <Link to="/">
-            <Button type="primary">Natrag</Button>
-        </Link>
+      <Header
+        style={{
+          borderBottom: 0,
+          borderTop: "2px",
+          backgroundColor: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between"
+        }}
+      >
+        <Logo />
+        <Button onClick={() => navigate(-1)}>Natrag</Button>
       </Header>
       <Layout>
-
+        <Content style={{ display: "flex", alignItems: "center", flexDirection: "column", flex: "1", width: "100%" }}>
+          <Typography.Title level={2}>Prijava</Typography.Title>
+          <Form labelCol={{ span: 7 }} wrapperCol={{ span: 20 }} style={{ width: "100%", maxWidth: "30em" }}>
+            <Form.Item
+              label="Korisničko ime: "
+              name="username"
+              rules={[{ required: true, message: "Molimo unesite svoje korisničko ime" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Lozinka: "
+              name="password"
+              rules={[{ required: true, message: "Molimo unesite svoju lozinku" }]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 10 }}>
+              <Button type="primary" htmlType="submit">Prijava</Button>
+            </Form.Item>
+          </Form>
+        </Content>
       </Layout>
     </Layout>
   );
