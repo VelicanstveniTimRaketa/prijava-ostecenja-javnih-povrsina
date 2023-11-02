d0063f41d504779c656297d790475fbbdb5ec11c
josipsare <josip.sare3@gmail.com>
2023-11-02 10:10:24 +0100

    proširene funkcionalnosti contollera

--- backend/src/main/java/com/backend/projectapi/controller/BaseController.java
+++ backend/src/main/java/com/backend/projectapi/controller/BaseController.java
@@ -1,5 +1,6 @@
 package com.backend.projectapi.controller;
 
+import com.backend.projectapi.exception.RecordNotFoundException;
 import com.backend.projectapi.model.Korisnik;
 import com.backend.projectapi.model.Prijava;
 import com.backend.projectapi.repository.KorisniciRepository;
@@ -8,18 +9,29 @@ import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BaseController {
+
     @Autowired
     PrijaveRepository repo;
 
     @GetMapping("/home")
-    public ResponseEntity<List<Prijava>> getUsers(){
+    public ResponseEntity<Object> getUsers(){
         return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/home/child_prijave")
+    public ResponseEntity<Object> getChildPrijave(@RequestBody Prijava prijava){
+        Optional<Prijava> parent_prijava= repo.findById(prijava.getId());
+        if (parent_prijava.isEmpty()){
+            throw new RecordNotFoundException("nepostoji prijava za dani id: "+prijava.getId());
+        }else {
+            return new ResponseEntity<>(repo.findAllByParentPrijava(prijava), HttpStatus.OK);
+        }
     }
 }
--- backend/src/main/java/com/backend/projectapi/repository/PrijaveRepository.java
+++ backend/src/main/java/com/backend/projectapi/repository/PrijaveRepository.java
@@ -3,5 +3,10 @@ package com.backend.projectapi.repository;
 import com.backend.projectapi.model.Prijava;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PrijaveRepository extends JpaRepository<Prijava, Long> {
+
+    Optional<List<Prijava>> findAllByParentPrijava(Prijava prijava);
 }
