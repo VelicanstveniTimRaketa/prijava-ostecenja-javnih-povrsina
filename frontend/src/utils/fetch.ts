d02de147fbe85829af1aea7cea4fcefad24bebc8
Dominik Zoricic <dominik.zoricic10000@gmail.com>
2023-11-04 22:19:23 +0100

    Frontend append fix

--- frontend/src/utils/fetch.ts
+++ frontend/src/utils/fetch.ts
@@ -4,46 +4,52 @@ import { BarebonesPrijava, Prijava, Response, TipOstecenja } from "./types";
 export type PrijaveOptions = {
   active?: string;
   ostecenjeId?: string;
-}
+};
 
-export function getPrijave(options?: PrijaveOptions): Promise<Response<Prijava[]>> {
-  return new Promise(res => {
+export function getPrijave(
+  options?: PrijaveOptions
+): Promise<Response<Prijava[]>> {
+  return new Promise((res) => {
     fetch("/api/prijave?" + new URLSearchParams(options))
-      .then(resp => resp.json())
-      .then(r => res(r))
-      .catch(error => res({ success: false, error }));
+      .then((resp) => resp.json())
+      .then((r) => res(r))
+      .catch((error) => res({ success: false, error }));
   });
 }
 
 export function getUserPrijave(id: string): Promise<Response<Prijava[]>> {
-  return new Promise(res => {
+  return new Promise((res) => {
     fetch("/api/prijave?" + new URLSearchParams({ id }))
-      .then(resp => resp.json())
-      .then(r => res(r))
-      .catch(error => res({ success: false, error }));
+      .then((resp) => resp.json())
+      .then((r) => res(r))
+      .catch((error) => res({ success: false, error }));
   });
 }
-
 
 export function getOstecenja(): Promise<Response<TipOstecenja[]>> {
-  return new Promise(res => {
+  return new Promise((res) => {
     fetch("/api/ostecenja")
-      .then(resp => resp.json())
-      .then(r => res(r))
-      .catch(error => res({ success: false, error }));
+      .then((resp) => resp.json())
+      .then((r) => res(r))
+      .catch((error) => res({ success: false, error }));
   });
 }
 
-export function addPrijava(prijava: BarebonesPrijava, images: RcFile[]): Promise<Response<never>> {
-  return new Promise(res => {
+export function addPrijava(
+  prijava: BarebonesPrijava,
+  images: RcFile[]
+): Promise<Response<never>> {
+  return new Promise((res) => {
     const data = new FormData();
-    
-    Object.entries(prijava).forEach(entry => data.append(entry[0], JSON.stringify(entry[1])));
-    images.forEach(im => data.append("files", im, im.name));
 
-    fetch("/api/addPrijava", { method: "POST", body: data, })
-      .then(resp => resp.json())
-      .then(r => console.log(r))
-      .catch(error => res({ success: false, error }));
+    Object.entries(prijava).forEach((entry) =>
+      data.append(entry[0], JSON.stringify(entry[1]))
+    );
+    images.forEach((im) => data.append("slike", im, im.name));
+
+    fetch("/api/addPrijava", { method: "POST", body: data })
+      .then((resp) => resp.json())
+      .then((r) => console.log(r))
+      .catch((error) => res({ success: false, error }));
   });
 }
