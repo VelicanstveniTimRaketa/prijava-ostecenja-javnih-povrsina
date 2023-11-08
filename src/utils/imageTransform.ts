e7dfd36a965003df60e7e3cd73e8a1b6c7e4a778
spinzed <davor.najev@gmail.com>
2023-11-08 03:17:43 +0100

    front: Remove unneeded files

--- src/utils/imageTransform.ts
+++
@@ -1,9 +0,0 @@
-import { RcFile } from "antd/es/upload";
-
-export const getBase64 = (img: RcFile): Promise<string> => {
-  return new Promise(res => {
-    const reader = new FileReader();
-    reader.addEventListener("load", () => res(reader.result as string));
-    reader.readAsDataURL(img);
-  });
-};
