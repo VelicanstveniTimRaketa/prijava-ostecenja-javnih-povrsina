5bdad527cc7f890afc522351b020a9d24f2d4f09
spinzed <davor.najev@gmail.com>
2023-11-08 03:09:38 +0100

    front: bugfix: Now buttons on welcome screen work

--- frontend/src/views/WelcomeView.tsx
+++ frontend/src/views/WelcomeView.tsx
@@ -3,11 +3,14 @@ import { Content } from "antd/es/layout/layout";
 import Title from "antd/es/typography/Title";
 import BigButton from "../components/BigButton";
 import MapJsApi from "../components/MapJsApi";
+import { useNavigate } from "react-router-dom";
 
 function WelcomeView() {
   const {
     token: { colorBgContainer },
   } = theme.useToken();
+
+  const navigate = useNavigate();
 
   return (
     <Layout
@@ -29,8 +32,8 @@ function WelcomeView() {
           Odmah i sada!
         </Typography.Paragraph>
         <Layout style={{ display: "flex", flexDirection: "row", gap: "1em" }}>
-          <BigButton bold>Pretraži</BigButton>
-          <BigButton bold>Prijavi</BigButton>
+          <BigButton onClick={() => navigate("search")} bold>Pretraži</BigButton>
+          <BigButton onClick={() => navigate("report")} bold>Prijavi</BigButton>
         </Layout>
       </Content>
       <Layout style={{ flex: 1, height: "500px", background: colorBgContainer }}>
