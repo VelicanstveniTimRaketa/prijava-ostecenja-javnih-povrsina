fa425f3f9aed232fe220efc81489e84595bcd8cb
josipsare <josip.sare3@gmail.com>
2023-11-13 14:43:00 +0100

    dodana metoda updatePrijave

--- backend/src/main/java/com/backend/projectapi/controller/PrijaveController.java
+++ backend/src/main/java/com/backend/projectapi/controller/PrijaveController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 
 import java.time.ZonedDateTime;
@@ -62,5 +64,10 @@ public class PrijaveController {
     @DeleteMapping("/deletePrijava")
     public ResponseEntity<Object> deletePrijava(@RequestParam Long id){
         return new ResponseEntity<>(ResponseData.success(prijavaService.deletePrijava(id)), HttpStatus.OK);
+    }
+
+    @PatchMapping("/updatePrijava")
+    public ResponseEntity<Object> updatePrijava(@RequestParam Long id, @RequestParam PrijavaDTO prijavaDTO,@RequestParam MultipartFile[] slike){
+        return new ResponseEntity<>(ResponseData.success(prijavaService.updatePrijava(id,prijavaDTO,slike)),HttpStatus.OK);
     }
 }
