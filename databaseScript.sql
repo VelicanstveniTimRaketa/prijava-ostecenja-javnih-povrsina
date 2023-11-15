f80332daa7dd77676c78e2e96fd72d4f480faa0d
Dominik Zoricic <dominik.zoricic10000@gmail.com>
2023-11-15 16:58:12 +0100

    Updated sql script

--- databaseScript.sql
+++ databaseScript.sql
@@ -1,47 +1,50 @@
-insert into public.korisnici (id,email,username,ime,prezime,password_hash,role,token) values
-                                                                                          (default, 'guest@gmail.com', 'guest','josip','Sare','','',''),
-                                                                                          (default, 'korisnik1@gmail.com', 'korisnik1','bigD','Najev','','',''),
-                                                                                          (default, 'korisnik2@gmail.com', 'korisnik2','smallD','Zoricic','','',''),
-                                                                                          (default, 'korisnik3@gmail.com', 'korisnik3','Nikola','Boticelli','','',''),
-                                                                                          (default, 'korisnik4@gmail.com', 'korisnik4','Ivan','Elezinjo','','','');
+insert into public.korisnici (email,username,ime,prezime,password,role) values
+                                                                            ('guest@gmail.com', 'guest','josip','Sare','','USER'),
+                                                                            ('korisnik1@gmail.com', 'korisnik1','bigD','Najev','','USER'),
+                                                                            ('korisnik2@gmail.com', 'korisnik2','smallD','Zoricic','','USER'),
+                                                                            ('korisnik3@gmail.com', 'korisnik3','Nikola','Boticelli','','USER'),
+                                                                            ('korisnik4@gmail.com', 'korisnik4','Ivan','Elezinjo','','USER');
 
 insert into public.lokacije (lokacija_id, latitude, longitude) values
-                                                                   (default, 45.81505, 15.98195),
-                                                                   (default, 45.3271, 14.4422),
-                                                                   (default, 45.5550, 18.6955),
-                                                                   (default, 43.5147, 16.4435),
-                                                                   (default, 45.4929, 15.5553),
-                                                                   (default, 45.81509, 15.98190),
-                                                                   (default, 45.81502, 15.98196);
+                                                                   (1, 45.81505, 15.98195),
+                                                                   (2, 45.3271, 14.4422),
+                                                                   (3, 45.5550, 18.6955),
+                                                                   (4, 43.5147, 16.4435),
+                                                                   (5, 45.4929, 15.5553),
+                                                                   (6, 45.81509, 15.98190),
+                                                                   (7, 45.81502, 15.98196);
 
-insert into public.tipovi_ostecenja (id, naziv) values
-                                                             (default, 'Oštećenje cestovne površine'),
-                                                             (default, 'Oštećenje cijevi na javnoj površini'),
-                                                             (default, 'Oštećenje elektrike nad cestom'),
-                                                             (default, 'Oštećenje vodovoda ispod javne površine');
+insert into public.tipovi_ostecenja (naziv) values
+                                                ('Oštećenje cestovne površine'),
+                                                ('Oštećenje cijevi na javnoj površini'),
+                                                ('Oštećenje elektrike nad cestom'),
+                                                ('Oštećenje vodovoda ispod javne površine');
 
-insert into public.gradski_uredi (id, ostecenje_id, naziv) values
-                                                 (default, 1, 'Vijece za ceste'),
-                                                 (default, 2, 'Vijece za vodovod'),
-                                                 (default, 3, 'Vijece za elektriku'),
-                                                 (default, 4, 'Vijece za cijevi');
+insert into public.gradski_uredi (ostecenje_id, naziv) values
+                                                           (1,'Vijece za ceste'),
+                                                           (2,'Vijece za vodovod'),
+                                                           (3,'Vijece za elektriku'),
+                                                           (4,'Vijece za cijevi');
 
 
 insert into public.prijave
-(id, ostecenje_id,opis, lokacija_id, kreator_id, prvo_vrijeme_prijave, parent_prijava_id, vrijeme_otklona) values
-                                                                                                               (default, 1,'', 1, 1, current_timestamp, null, null),
-                                                                                                               (default, 2,'', 3, 1, current_timestamp, 1, null),
-                                                                                                               (default, 2,'', 2, 2, current_timestamp, null, null),
-                                                                                                               (default, 3,'', 6, 3, current_timestamp, 1, null),
-                                                                                                               (default, 2,'', 6, 2, current_timestamp - '10 hours' :: interval, null, current_timestamp),
-                                                                                                               (default, 2,'',2,4,current_timestamp - '1 day'::interval,2,null),
-                                                                                                               (default, 3,'', 7, 1, current_timestamp - '2 hours' :: interval, null, null),
-                                                                                                               (default, 4,'', 7, 2, current_timestamp - '5 hours' :: interval, 2, null),
-                                                                                                               (default, 4,'', 4, 3, current_timestamp - '12 hours' :: interval, null, current_timestamp),
-                                                                                                               (default, 3,'', 2, 4, current_timestamp - '3 days' :: interval, 1, null),
-                                                                                                               (default, 4,'', 4, 4, current_timestamp - '7 days' :: interval, null, current_timestamp - '1 day' :: interval),
-                                                                                                               (default, 3,'', 3, 1, current_timestamp - '10 days' :: interval, 2 , null);
+(ostecenje_id,opis, lokacija_id, kreator_id, prvo_vrijeme_prijave, parent_prijava_id, vrijeme_otklona) values
+                                                                                                           (1,'', 1, 1, current_timestamp, null, null),
+                                                                                                           (2,'', 3, 1, current_timestamp, 1, null),
+                                                                                                           (2,'', 2, 2, current_timestamp, null, null),
+                                                                                                           (3,'', 6, 3, current_timestamp, 1, null),
+                                                                                                           (2,'', 6, 2, current_timestamp - '10 hours' :: interval, null, current_timestamp),
+                                                                                                           (2,'',2,4,current_timestamp - '1 day'::interval,2,null),
+                                                                                                           (3,'', 7, 1, current_timestamp - '2 hours' :: interval, null, null),
+                                                                                                           (4,'', 7, 2, current_timestamp - '5 hours' :: interval, 2, null),
+                                                                                                           (4,'', 4, 3, current_timestamp - '12 hours' :: interval, null, current_timestamp),
+                                                                                                           (3,'', 2, 4, current_timestamp - '3 days' :: interval, 1, null),
+                                                                                                           (4,'', 4, 4, current_timestamp - '7 days' :: interval, null, current_timestamp - '1 day' :: interval),
+                                                                                                           (3,'', 3, 1, current_timestamp - '10 days' :: interval, 2 , null);
 
 
 select * from korisnici;
-select prijave.* from prijave natural join lokacije where latitude between (45.3271 - 0.0005) and (45.3271 + 0.0005) and longitude between (14.4422 - 0.0005) and (14.4422 + 0.0005);
+select prijave.* from prijave natural join lokacije where latitude between (45.3271 - 0.00009) and (45.3271 + 0.00009) and longitude between (14.4422 - 0.00009) and (14.4422 + 0.00009);
+
+
+select * from prijave
