a8a0f7ef7cfbd660379f2c5393104e2613b9726e
spinzed <davor.najev@gmail.com>
2023-11-16 18:34:21 +0100

    front: Initial deployment config

---
+++ frontend/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+dotenv.config();
+
+const app = express();
+
+// Configuration
+const { PORT } = process.env;
+const { HOST } = process.env;
+const { API_BASE_URL } = process.env;
+
+// Proxy
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: API_BASE_URL,
+    changeOrigin: true,
+  })
+);
+
+const staticPath = join(dirname(fileURLToPath(import.meta.url)), "dist");
+
+app.use(express.static(staticPath))
+
+app.listen(PORT, HOST, () => {
+  console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
+
+app.get("*", async (req, res) => {
+  res.sendFile(join(staticPath, "index.html"))
+});
