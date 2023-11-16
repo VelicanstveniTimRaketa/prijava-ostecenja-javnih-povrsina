1577b1c9f75025dd444cfb78193460626f428732
spinzed <davor.najev@gmail.com>
2023-11-16 16:53:47 +0100

    back: Add debug prints

--- backend/src/main/java/com/backend/projectapi/BackendApplication.java
+++ backend/src/main/java/com/backend/projectapi/BackendApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println("#############");
+		System.out.println(System.getenv("DB_URL"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
