05f1d9eddc06bb506e0aa3a965d8d348b457520a
josipsare <josip.sare3@gmail.com>
2023-12-21 20:57:02 +0100

    back: feat: added new func to GradskiUred

--- backend/src/main/java/com/backend/projectapi/repository/GradskiUrediRepository.java
+++ backend/src/main/java/com/backend/projectapi/repository/GradskiUrediRepository.java
@@ -1,7 +1,17 @@
 package com.backend.projectapi.repository;
 
+import com.backend.projectapi.DTO.GradskiUredDTO;
 import com.backend.projectapi.model.GradskiUred;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GradskiUrediRepository extends JpaRepository<GradskiUred, Long> {
+
+
+    @Query(value = "select * from gradski_uredi g where g.active = 'false'  ", nativeQuery = true)
+    List<GradskiUred> findNeaktiveUrede();
+    @Query(value = "select * from gradski_uredi g where g.active = 'true'  ", nativeQuery = true)
+    List<GradskiUred> findAktivneUrede();
 }
--- backend/src/main/java/com/backend/projectapi/service/GradskiUrediService.java
+++ backend/src/main/java/com/backend/projectapi/service/GradskiUrediService.java
@@ -1,10 +1,15 @@
 package com.backend.projectapi.service;
 
+import com.backend.projectapi.DTO.GradskiUredDTO;
 import com.backend.projectapi.model.GradskiUred;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface GradskiUrediService {
-    public List<GradskiUred> getAllUredi();
+
+    List<GradskiUred> getAll(String active);
+    Object addGradskiUred(GradskiUredDTO gradskiUredDTO);
+
+    Object makeActive(Long id);
 }
