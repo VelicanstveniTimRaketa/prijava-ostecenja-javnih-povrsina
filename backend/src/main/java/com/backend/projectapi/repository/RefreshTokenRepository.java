446496e7f481c56bcda242643fed47d660f6b098
josipsare <josip.sare3@gmail.com>
2024-01-11 21:28:50 +0100

    back: fix: fixed error relating to RefreshToken

--- backend/src/main/java/com/backend/projectapi/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/backend/projectapi/repository/RefreshTokenRepository.java
@@ -4,13 +4,19 @@ import com.backend.projectapi.model.Korisnik;
 import com.backend.projectapi.model.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Ref;
 import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
+
+    @Query(value = "select * from refreshtoken where user_id=:id ", nativeQuery = true)
+    Optional<RefreshToken> findByKorisnikId(@Param("id") Long id);
 
     @Modifying
     int deleteByUser(Korisnik user);
