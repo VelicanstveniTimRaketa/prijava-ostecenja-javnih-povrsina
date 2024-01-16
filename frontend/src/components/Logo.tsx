936560719832145a9ea87260a403f5e484651891
spinzed <davor.najev@gmail.com>
2024-01-16 15:39:42 +0100

    front: feat: Update logo

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -1,11 +1,11 @@
 import { Image } from "antd";
-import LogoImage from "../assets/logo.svg";
 import { Link } from "react-router-dom";
+import LogoImage from "../assets/logo.webp";
 
 function Logo() {
   return (
     <Link to="/">
-      <Image src={LogoImage} preview={false} alt="Logo Image" />
+      <Image src={LogoImage} preview={false} alt="Logo Image" style={{ height: "2.5em" }} />
     </Link>
   );
 }
