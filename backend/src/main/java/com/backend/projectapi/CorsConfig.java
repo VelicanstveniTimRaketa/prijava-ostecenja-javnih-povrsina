eddbf686fbfa761e936cbc7d2709ef82c56c1905
spinzed <davor.najev@gmail.com>
2024-01-19 16:23:29 +0100

    back: fix: Fix JWT token loading

--- backend/src/main/java/com/backend/projectapi/CorsConfig.java
+++ backend/src/main/java/com/backend/projectapi/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.backend.projectapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins(System.getenv("CLIENT_URL"))
+                .allowedOrigins(Dotenv.load().get("CLIENT_URL"))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
                 .allowCredentials(true);
